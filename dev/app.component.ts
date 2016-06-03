35f01f50c709ef82b2179e334f709767f34ce7d5
halokin <halokin@gmail.com>
2016-06-03 21:11:29 +0200

    two way data binding

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -8,6 +8,9 @@ import {Component} from 'angular2/core';
         <br>
         <input type="text" [value]="name" [ngClass]="{red: true}" (keyup)="onKeyup(inputElement.value)" #inputElement>
         <p>{{values}}</p>
+        <br><br>
+        <input type="text" [(ngModel)]="name">
+        <p>Your Name: {{name}}</p>
     `,
 })
 export class AppComponent {
