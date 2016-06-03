4ac8f3dba73a6b79bc3beebd25b8c42e710c69bc
halokin <halokin@gmail.com>
2016-06-03 21:03:07 +0200

    event binding

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -3,10 +3,22 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'my-app',
     template: `
-        <h1>Angular 2 Boilerplate</h1>
-        <p>Hello World!</p>
+        {{onTest()
+        }}
+        <br>
+        <input type="text" [value]="name" [ngClass]="{red: true}" (keyup)="onKeyup(inputElement.value)" #inputElement>
+        <p>{{values}}</p>
     `,
 })
 export class AppComponent {
+ name = 'Nicolas';
+ values ='';
 
+ onTest(){
+ return 1 === 1;
+ }
+
+ onKeyup(value: string) {
+ this.values += value + ' | ';
+ }
 }
